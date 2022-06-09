68905d5e347f5b7ac1d004f7d20992ee9bfcfad8
Shaalan Marwan <salsanjary@gmail.com>
2022-06-09 16:22:26 +0300

    tree-bfs-without-recursion

---
+++ tree-bfs-without-recursion/tree-bfs-without-recursion.ts
@@ -0,0 +1,30 @@
+
+class Tree<T> {
+    value: T;
+    left: Tree<T>;
+    right: Tree<T>;
+  
+    constructor(value: T) {
+      this.value = value;
+      this.left = null;
+      this.right = null;
+    }
+  }
+  function solution(t: Tree<number>): number[] {
+       if (!t) return [];
+       const res: number[] = [];
+       const queue: Tree<number>[] = [t];
+       while (queue.length) {
+           const len = queue.length;
+           for (let i = 0; i < len; i++) {
+            const node = queue.shift();
+            res.push(node!.value);
+           if (node!.left) queue.push(node!.left);
+           if (node!.right) queue.push(node!.right);
+       }
+   }
+      
+      return res;
+  
+  }
+  
